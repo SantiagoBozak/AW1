f8ad8d05f72290365cc93d45fc067afab94b5cf1
SantiagoBozak <hernibozak@gmail.com>
2026-05-25 11:14:42 -0300

    navbar dinamico y correcciones de las cards

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,75 @@
 const paginas = [
-    { titulo: "Home", link: "/index.html" },
-    { titulo: "Wireless", link: "/page/categoria1.html" },
-    { titulo: "Wired", link: "/page/categoria2.html" },
-    { titulo: "Gaming", link: "/page/categoria3.html" }
+  { titulo: "Home", rutaRoot: "index.html", rutaPage: "../index.html" },
+
+  { titulo: "Wireless", rutaRoot: "./page/categoria1.html", rutaPage: "./categoria1.html" },
+
+  { titulo: "Wired", rutaRoot: "./page/categoria2.html", rutaPage: "./categoria2.html" },
+
+  { titulo: "Gaming", rutaRoot: "./page/categoria3.html", rutaPage: "./categoria3.html" },
+
+  { titulo: "Login", rutaRoot: "./page/login.html", rutaPage: "./login.html" },
+
+  { titulo: "Registro", rutaRoot: "./page/registro.html", rutaPage: "./registro.html" }
 ];
 
-console.log(paginas);
+function crearNavbar() {
+
+  const navbar = document.getElementById("navbar");
+
+  if (!navbar) return;
+
+  const estamosEnPage = window.location.pathname.includes("/page/");
+
+  let contenido = `
+    <nav>
+      <div class="container">
+        <h1 class="logo">👌 Mickey Mouses 👌</h1>
+        <ul>
+  `;
+
+  paginas.forEach(pagina => {
+
+    const ruta = estamosEnPage
+      ? pagina.rutaPage
+      : pagina.rutaRoot;
+
+    contenido += `
+      <li>
+        <a href="${ruta}">
+          ${pagina.titulo}
+        </a>
+      </li>
+    `;
+  });
+
+  const usuario = sessionStorage.getItem("usuario");
+
+if (usuario) {
+
+  contenido += `
+    <li>
+      <a href="#" id="logoutBtn">Logout</a>
+    </li>
+  `;
+
+}
+
+  navbar.innerHTML = contenido;
+
+  const logoutBtn = document.getElementById("logoutBtn");
+
+  if (logoutBtn) {
+
+    logoutBtn.addEventListener("click", () => {
+
+      sessionStorage.removeItem("usuario");
+
+      window.location.href = "/page/login.html";
+
+    });
+
+  }
+
+}
+
+crearNavbar();
